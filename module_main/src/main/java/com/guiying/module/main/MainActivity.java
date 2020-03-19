a78b904f7695920c420ea991210d33fc3cd3de13
wangyongsheng <wangyongsheng@01zhuanche.com>
2020-03-19 17:29:18 +0800

    update

--- module_main/src/main/java/com/guiying/module/main/MainActivity.java
+++ module_main/src/main/java/com/guiying/module/main/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.guiying.module.common.base.BaseActivity;
 import com.guiying.module.common.base.ViewManager;
 import com.guiying.module.common.utils.ToastUtils;
+import com.guiying.module.common.utils.Utils;
 
 /**
  * <p>类说明</p>
@@ -28,6 +30,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         findViewById(R.id.news_button).setOnClickListener(this);
         findViewById(R.id.girls_button).setOnClickListener(this);
         findViewById(R.id.fragment_button).setOnClickListener(this);
+
+        Toast.makeText(getApplicationContext(), "------------s", Toast.LENGTH_LONG).show();
     }
 
     @Override
